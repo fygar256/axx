ed2bba1fd7f611b4452967db2595ce68f83df243
Taisuke Maekawa <fygar256@gmail.com>
2026-01-31 13:32:53 +0900

    Add files via upload

---
+++ hello.bf.s
@@ -0,0 +1,117 @@
+inc *p
+inc *p
+inc *p
+inc *p
+inc *p
+inc *p
+inc *p
+inc *p
+inc *p
+inc *p
+eol
+while *p
+eol
+inc p
+inc *p
+inc *p
+inc *p
+inc *p
+inc *p
+inc *p
+inc *p
+inc p
+inc *p
+inc *p
+inc *p
+inc *p
+inc *p
+inc *p
+inc *p
+inc *p
+inc *p
+inc *p
+inc p
+inc *p
+inc *p
+inc *p
+inc p
+inc *p
+dec p
+dec p
+dec p
+dec p
+dec *p
+eol
+eol
+wend
+eol
+inc p
+inc *p
+inc *p
+output
+inc p
+inc *p
+output
+inc *p
+inc *p
+inc *p
+inc *p
+inc *p
+inc *p
+inc *p
+output
+output
+inc *p
+inc *p
+inc *p
+output
+inc p
+inc *p
+inc *p
+output
+dec p
+dec p
+inc *p
+inc *p
+inc *p
+inc *p
+inc *p
+inc *p
+inc *p
+inc *p
+inc *p
+inc *p
+inc *p
+inc *p
+inc *p
+inc *p
+inc *p
+output
+inc p
+output
+inc *p
+inc *p
+inc *p
+output
+dec *p
+dec *p
+dec *p
+dec *p
+dec *p
+dec *p
+output
+dec *p
+dec *p
+dec *p
+dec *p
+dec *p
+dec *p
+dec *p
+dec *p
+output
+inc p
+inc *p
+output
+inc p
+output
+eol
