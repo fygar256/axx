daf3818661b2679208d5a35dea0327abb1817e22
Taisuke Maekawa <fygar256@gmail.com>
2026-02-05 01:00:45 +0900

    Add files via upload

--- test.s
+++ test.s
@@ -1,4 +1,6 @@
 ldf a,flt{3.14}
+ldd a,dbl{3.14}
+ldq a,qad{3.14}
 leaq rax , [ rbx , rcx , 2 , 0x40]
 leaq rax , [ rbx + rcx * 2 + 0x40 ]
 addi $v0,$a0,5
@@ -7,3 +9,4 @@ add r1, r2, r3 lsl #20
 rep movsb
 movsb
 load a,[b]
+repeat 10
