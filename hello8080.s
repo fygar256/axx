950244cc8e85b87c77dd819ee0a6938001d14da5
fygar256 <fygar256@gmail.com>
2026-09-25 12:24:59 +0900

    2026-09-25-12

---
+++ hello8080.s
@@ -0,0 +1,8 @@
+        .org 0x100        ; .COM は 0x100 にロードされる
+start:
+        mvi c,9          ; BDOS function 9 = print $-terminated string
+        lxi d,msg        ; DE = アドレス of msg
+        call 0x0005      ; BDOS entry at 0005h
+        ret              ; CP/M に戻る
+
+msg:    db 'Hello, world$'
